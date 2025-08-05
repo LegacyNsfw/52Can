6a52c4d1994e4ab65b3162f487e39179760970ca
NSFW <nsfw@outlook.com>
2025-08-04 22:16:18 -0700

    Cleaned up display code a bit.

--- Software/DisplayComponent.cpp
+++ Software/DisplayComponent.cpp
@@ -13,9 +13,6 @@ Arduino_GFX *canvas = nullptr;
 // green -> blue
 // blue -> very light blue
 
-#define SWAPXY_DISPLAY
-// #define SWAPXY_CANVAS
-
 int count;
 
 void DisplayComponent::initialize()
@@ -25,8 +22,6 @@ void DisplayComponent::initialize()
   bus =  new Arduino_ESP32SPI(DC, CS, SCLK, MOSI, BL);
   
   Serial.println("Creating display...");
-
-#ifdef SWAPXY_DISPLAY
   display = new Arduino_ST7735(
     bus,
     2, //  RST
@@ -35,37 +30,10 @@ void DisplayComponent::initialize()
     height, // x
     width // y
   );
-#else
-  display = new Arduino_ST7735(
-    bus,
-    2, //  RST
-    0, // rotation - this is what unswaps the x and y below
-    true, // ips
-    width,
-    height
-  );
-#endif  
 
   Serial.println("Creating canvas...");
-#ifdef SWAPXY_CANVAS
-  canvas = new Arduino_Canvas(height, width, display, 0, 0, 0);
-#else
   canvas = new Arduino_Canvas(width, height, display, 0, 0, 0);
-#endif
-
-
-  // The Adafruit ST7735 breakout board is natively in portrait mode, and this app wants landscape.
-  // Rotation experiments:
-  // display: width, height, r=0; canvas: width, height, r=0; stripe on left does not get updated
-  // display: width, height, r=0; canvas: height, width, r=0; stripe on left does not get updated
-  // display: width, height, r=0; canvas: height, width, r=3; stripe on left does not get updated
-  // display: height, width, r=0; canvas: width, height, r=3; stripe on left does not get updated
-  // display: height, width, r=0; canvas: height, width, r=3; stripe on left does not get updated
-  // display: width, height, r=3; canvas: width, height, r=3; stripe on right does not get updated
-  // display: height, width, r=3; canvas: height, width, r=0; full screen works
-  // display: height, width, r=3; canvas: width, height, r=0; doesn't work, hard to describe
-  
-  
+    
   Serial.println("Starting GFX...");
   int speed = 12 * 1000 * 1000;
   
@@ -76,9 +44,10 @@ void DisplayComponent::initialize()
   // This works, but not if you use a canvas.
   // display->begin(speed);
 
-  // This allocates memory for the canvas framebuffer, and calls begin() on the display.
+  // This allocates memory for the canvas framebuffer, and calls begin() on the display, and initializes the bus.
   canvas->begin(speed);
 
+  Serial.println("Clearing display...");  
   display->fillScreen(RGB565_BLACK);
   Serial.println("Display initialized.");  
 }
@@ -97,49 +66,8 @@ void DisplayComponent::drawHistory(History *pHistory, uint16_t temperature)
 {
   for (int x = 0; x < width; x++)
   {
-    // Draws a diagonal line
-    //double fraction = (double)x / (double)height;
-    //int y = fraction * width;
-    //display->drawPixel(y, x, RGB565_BLACK);
-
     double fraction = (double)x / (double)width;
     int y = fraction * height;
     canvas->drawPixel(x, y, RGB565_BLACK);
-
-/*    int y = pHistory->get(width - x);
-    canvas->drawPixel(height, x, RGB565_BLACK);
-
-    if (y < height - 1)
-      canvas->drawPixel(height+1, x, RGB565_BLACK);
-  }*/
   }
 }
-
-
-/*
-void DisplayComponent::drawHistoryTop(History *pHistory, uint16_t color)
-{
-  for (int x = 0; x < width; x++)
-  {
-    int y = pHistory->get(width - x);
-    if (y > height / 2)
-    {
-      canvas->drawPixel(x, height - y, color);
-      canvas->drawPixel(x, (height - y) - 1, color);
-    }
-  }
-}
-
-void DisplayComponent::drawHistoryBottom(History *pHistory, uint16_t color)
-{
-  for (int x = 0; x < width; x++)
-  {
-    int y = pHistory->get(width - x);
-    if (y <= height / 2)
-    {
-      canvas->drawPixel(x, height / 2 - y, color);
-      canvas->drawPixel(x, (height / 2 - y) - 1, color);    
-    }
-  }
-}
-*/
